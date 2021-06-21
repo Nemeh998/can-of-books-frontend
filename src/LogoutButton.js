c4be334d5e398012a48a66674591e4d186ed0637
nemh <nemh998@gmail.com>
2021-06-21 15:11:40 +0300

    solve lab 11

--- src/LogoutButton.js
+++ src/LogoutButton.js
@@ -1,12 +1,18 @@
-import React from "react";
-import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
-  const { isAuthenticated,loginWithRedirect, } = useAuth0();
-console.log(isAuthenticated);// 
-  return  isAuthenticated &&(
-   <button onClick={() => loginWithRedirect()}>Log In</button>
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+function LogoutButton() {
+  const {
+    isAuthenticated,
+    logout,
+  } = useAuth0();
+
+  return isAuthenticated && (
+    <button onClick={() => {
+      logout({ returnTo: window.location.origin });
+    }}>Log out</button>
   );
 }
 
-export default LoginButton;
+export default LogoutButton;
